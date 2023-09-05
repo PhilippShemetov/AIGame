9bd80680d94a3c4da0a83f038d464215dbc21eb5
Philipp <filshe1512@gmail.com>
2023-09-05 22:46:52 +0300

    Initial commit

---
+++ Source/FntasticProject/FntasticProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FntasticProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FNTASTICPROJECT_API AFntasticProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
